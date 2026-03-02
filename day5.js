5007bc6ff0157cb0f73fa97a55bf96ea0ff970cf
deekshi275 <deekshithdee018@gmail.com>
2026-03-02 10:16:22 +0530

    objects in js

---
+++ day5.js
@@ -0,0 +1,22 @@
+let person = {
+    name : "deekshi",
+    contact : 1234567890,
+    age : 22,
+    hob : ['dance','watchin movies','playing chess','pubg']
+}
+let a = (person.hob)
+
+console.log(`hello my name is ${person.name} \n my age is ${person.age}\n contatac ${person.contact} \n my hobies are ${person.hob}`)
+
+let person1 = {
+    detiles : {
+        name :'deekshi',
+        age : 20,
+        contact:123456789,
+        address : 'tumkur',
+        qulificatin : 'be'
+
+    },
+    hob :['dance','watchin movies','playing chess','pubg']
+}
+console.log(`hello my  name is : ${person.detiles.name} \nage is : ${person.detiles.age}\ncontact no is: ${person.detiles.contact}\nmy address is : ${person.detiles.address}\n and hobies are ${person.hob}`)
