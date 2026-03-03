8bfb4297ac696d51e5dee5049bdfd33c553e041c
deekshi275 <deekshithdee018@gmail.com>
2026-03-03 18:01:34 +0530

    objects in js

---
+++ day3.js
@@ -0,0 +1,38 @@
+let student = {
+  name: "deekshi",
+  age: "22",
+  gender: "male",
+};
+// console.log(student.name)
+// student.city = 'hassan'
+// delete student.age
+// console.log(student.city)
+// console.log(student.age)
+// console.log(Object.keys(student).length)
+// console.log(Object.values(student).length)
+// console.log('model' in student)
+
+// for (let i in student){
+//     console.log(i,student[i])
+// }
+
+let a = [
+  { name: "deekshi", age: 22, marks: 35 },
+  { name: "rakshi", age: 23, marks: 34 },
+  { name: "poo", age: 24, marks: 55 },
+  { name: "priya", age: 25, marks: 56 },
+  { name: "praveen", age: 26, marks: 45 },
+];
+
+
+let d = {name:'shu',age:23,marks:78}
+console.log(a.unshift(d))
+
+// for(let i=0;i<=a.length-1;i+=1){
+//     console.log(a[i].age)
+// }
+// console.log(a[0])
+
+
+console.log(...a)
+console.log(a[0])
