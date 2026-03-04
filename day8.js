1ace7525ab46b72b2559a86312f380b00bc69e35
deekshi275 <deekshithdee018@gmail.com>
2026-03-04 20:53:12 +0530

    functions

--- day8.js
+++ day8.js
@@ -1,2 +1,196 @@
-//---------------------------functions-----------------------------//
+// //---------------------------functions-----------------------------//
+// // function greet(){
+// //     console.log('hello this is deekhi')
+// //     console.log('iam deekshi')
+// //     console.log('happy holi')
+// // }
+// // greet()
+
+
+// // function add(a =10,b = 20){
+// //     console.log(a+b)
+// // }
+// // add()
+
+// // function sub(a = prompt('enter a number',b = prompt('enter a second number'))){
+// //     console.log(a-b)
+// // }
+// // sub()
+
+// // function add(){
+// //     let a   = 100
+// //     let b = 200
+// //     console.log(a + b)
+
+// // }
+// // add()
+
+// // function sub(){
+// //     let a = 100
+// //     let b = 200
+// //     console.log(a-b)
+
+// // }
+// // sub()
+// // a = 'jshgj'
+// // b ='jksfhuh'
+// // console.log(a)
+
+// function cancat(a = 'deekshui',b = 'BH'){
+//     console.log(a.concat(b))
+
+// }
+// cancat()
+
+// function find(a = 'deekshi',b = 'k'){
+//     console.log(a.indexOf(b))
+// }
+// find()
+
+// function slice(a = 'deekshi'){
+//     console.log(a.slice(1,5))
+// }   
+// slice()
+
+// function replace(a = 'deekshi'){
+//     console.log(a.replace('dee','ra'))
+// }
+// replace()
+
+// function rev(a = 'deekshi'){
+//     console.log(a.split('').reverse().join(''))
+// }
+// rev()
+
+// function lastindex(a='deekshi' ){
+//     console.log(a.lastIndexOf(i))
+// }
+// function repet(a ='deekshi'){
+//     console.log(a.repeat(5))
+// }
+// repet()
+
+// function padend(a = 'deekshi'){
+//     console.log(a.padEnd(10,"x"))
+// }
+// padend()
+
+
+// // function padstar(a = 'deekshi'){
+// // daconsole.log(a.padStart(6,'x'))
+// // }
+// // padstar()
+
+// console.log('----------------------------------------')
+
+
+// function add(a=10,b =20){
+//     console.log(a+b,'this is addtion')
+// }
+// add()
+
+// function sub(a=10,b=20){
+//     console.log(a-b,'this is substraction')
+// }
+// sub()
+
+// function mul(a=10,b=20){
+//     console.log(a*b,'this is multiplication')
+// }
+// mul()
+
+// function div(a=10,b=20){
+//     console.log(a/b,'this is division')
+// }
+// div()
+
+// function mod(a=10,b=20){
+//     console.log(a%b,'this is modulus')
+// }   
+// mod()
+
+// function flor(a =10,b =2){
+//     console.log(a**b)
+// }
+// flor()
+// console.log('----------------------------------------')
+// function assinment(a =10,b =20){
+//     console.log(a+=b)
+//     console.log(a-=b)
+//     console.log(a*=b)
+//     console.log(a/=b)
+//     console.log(a%=b)
+// }
+// assinment()
+
+// console.log('-------------------------------------------')
+
+// function comparison(a=10,b=20){
+//     console.log(a==b)
+//     console.log(a<b)
+//     console.log(a>b)
+//     console.log(a<=b)
+//     console.log(a>=b)
+//     console.log(a!=b)
+// }
+// comparison()
+
+
+// let arr = ['apple','banana','orange','mongo']
+// let arr1 = [9,4,1,2,3,4,5,6,7,8]
+// console.log(arr)
+
+// function concat(){
+//     console.log(arr.concat(arr1))
+// }
+// concat()
+
+// function reverse(){
+//     console.log(arr.reverse())
+// }
+// reverse()
+
+// function sort(){
+//     console.log(arr1.sort())
+// }
+// sort()
+
+// function push(){
+//     console.log(arr.push('apple2'))
+// }
+// push()
+
+// function findindex(){
+//  console.log(arr.indexOf('apple'))   
+// }
+// findindex()
+
+// function pop(){
+//     console.log(arr.pop())
+// }
+// pop()
+
+// function shift(){
+//     console.log(arr.shift())
+// }
+// shift()
+// function unshift(){
+//     console.log(arr.unshift('apple3'))
+// }
+// unshift()
+
+
+function add(a,b){
+    console.log(a+b)
+}
+add(10,20)
+
+
+let a = 'deekshi'
+let b = 'bhsdfghj'
+
+function can(a,b){
+    console.log(a.concat(b))
+}
+can(a,b)
 
