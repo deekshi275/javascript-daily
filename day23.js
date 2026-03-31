787de070f2d9e7d575e0e0e001d3f7e95759b2d9
deekshi275 <deekshithdee018@gmail.com>
2026-03-31 10:07:43 +0530

    day21

---
+++ day23.js
@@ -0,0 +1,29 @@
+// function changeText(){
+//     document.getElementById('h1').innerText='i learn dom'
+// }
+
+// function fun1(){
+//     document.getElementById('a').innerText='2+2'
+// }
+
+// function fun(){
+//     document.getElementById('wel').innerText='wellcome'
+// }
+
+// function fun(){
+//     document.getElementById('p').innerText='deekshi'
+// }
+let b = document.getElementById('btn')
+b.style.backgroundColor='green'
+
+
+function fun(){
+    let x = document.getElementById('p')
+    x.style.color='red'
+    x.style.fontSize='100px'
+    x.style.border = '30px solid black'
+    x.style.textAlign='center'
+    return x
+}
+
+
