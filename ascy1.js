4a8c47421a851b0540bd993c8a0c53fb89d270f5
deekshi275 <deekshithdee018@gmail.com>
2026-04-21 08:35:11 +0530

    updeted

--- ascy1.js
+++ ascy1.js
@@ -1,41 +1,8 @@
-let cards=document.getElementsByClassName("cards")[0]
-
-
-let task= async()=>{
-    let url="https://dummyjson.com/todos"
-    let data=  await fetch(url)
-    let d= await data.json()
-    let fd= d.todos
-    console.log(fd)
-
-    fd.map((ele)=>{
-        let {id,todo,completed,userId}= ele
-        if (completed){
-            cards.innerHTML+=`<div style="background-color: lightblue" class="card">
-                <h3>id: ${id} </h3>
-                <h3>todo: ${todo}</h3>
-                <h3 >userid :${userId}</h3>
-                <h3>completed :${completed}</h3>
-            </div>`
-
-        }
-        else{
-            cards.innerHTML+=`
-            <div style="background-color: lightgray;" class="card">
-                <h3>id: ${id} </h3>
-                <h3>todo: ${todo}</h3>
-                <h3 >userid: ${userId}</h3>
-                <h3>completed: ${completed}</h3>
-                
-            </div>
-        `
-        }
-    })
-
+function outer() {
+  let count = 0;
+  return function() {
+    count++;
+    return count;
+  };
 }
-task()
-
-
-
-
-// completed ? "true": "false" 0
+outer()
